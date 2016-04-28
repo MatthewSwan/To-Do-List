7c913f9c5a04a6e854bd2aa81612afbdb23fac86
Matthew Swan <swanm86@gmail.com>
2016-04-27 19:31:18 -0500

    can now update to do items

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -4,8 +4,25 @@ class ListController < ApplicationController
     @list = List.all
   end
 
+  def show
+    @list = List.find(params[:id])
+  end
+
   def create
     @list = List.create(:item => params[:item])
+    redirect_to root_path
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    List.update(params[:id], {item: params[:list][:item]})
     redirect_to root_path
   end
 end
