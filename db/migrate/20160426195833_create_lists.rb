ec3bcb11f73f125d03e346812353ffac0be9e636
Dongmin Lee <medi8600@gmail.com>
2016-04-29 13:54:43 -0500

    DATABASE SET~!!

--- db/migrate/20160426195833_create_lists.rb
+++ db/migrate/20160426195833_create_lists.rb
@@ -3,5 +3,14 @@ class CreateLists < ActiveRecord::Migration
     create_table :lists do |t|
       t.text :item
     end
+
+    create_table :entries do |t|
+      t.text "entry"
+    end
+
+    create_table :associations do |t|
+      t.integer :list_id
+      t.integer :entry_id
+    end
   end
 end
